d7994d66d2d7a2593ed4e02593d5486b82bfcb94
drecla-cloudflare <drecla-cloudflare>
2026-09-09 16:26:27 +0800

    R2 added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
+
+    // File upload/download via R2
+    if (url.pathname === '/file') {
+      if (request.method === 'POST') {
+        const file = await request.arrayBuffer();
+        await env.BANK_BUCKET.put('uploaded-file.bin', file);
+        return new Response('Uploaded!');
+      }
+
+      const object = await env.BANK_BUCKET.get('uploaded-file.bin');
+      if (!object) {
+        return new Response('Not found', { status: 404 });
+      }
+
+      return new Response(object.body);
+    }
+
     const key = url.searchParams.get('key');
     const value = url.searchParams.get('value');
 
