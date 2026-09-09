892c27ecd6da2ae0cb55a2cec09d93aa50ee1d4e
drecla-cloudflare <drecla-cloudflare>
2026-09-09 10:35:25 +0800

    second commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 javascript
 export default {
   async fetch(request, env, ctx) {
-    return new Response('Hello world! this is my first worker deployed in Github!', {
+    return new Response('Hello world! this is my first worker deployed in Github 2!', {
       headers: { 'Content-Type': 'text/plain' }
     });
   }
