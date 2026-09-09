03926a81f7bb5026fab46f470baeb1d92d3ca844
drecla-cloudflare <drecla-cloudflare>
2026-09-09 13:57:13 +0800

    back to main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env, ctx) {
     const data = {
-      message: 'Hello from Cloudflare Workers branch 22!',
+      message: 'Hello from Cloudflare Workers Main!!',
       timestamp: new Date().toISOString(),
       path: new URL(request.url).pathname
     };
